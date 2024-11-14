392591a884454d8107035e578a297697f8d9129b
Lucas Nazário <lucaskenpachi57@gmail.com>
2024-11-14 13:58:24 -0300

    condicional simples
    incluindo uma condicional simples

--- quiz.py
+++ quiz.py
@@ -1,6 +1,15 @@
 from InquirerPy import inquirer
-clubs = inquirer.select(
-    message='Qual o seu time do coração?',
-    choices=['Internacional','Grêmio','Flamengo','Palmeiras','Cruzeiro','Atlético Mineiro']
+campeao_cdb = inquirer.select(
+    message='Quem é o maior campeão da Copa do Brasil?',
+    choices=['Grêmio','Flamengo','Palmeiras','Cruzeiro','Atlético Mineiro']
 ).execute()
-print(f'Você é torcedor do {clubs}')
+if campeao_cdb == 'Cruzeiro': 
+    print('Você acertou, o Cruzeiro é maior campeão da copa do Brasil com 6 titulos!')
+else: print('Você errou, tente novamente!')
+
+'''campeao_br = inquirer.select(
+    message='Quem é o maior campeão do Campeonato Brasileiro?(Incluindo taça Brasil)'
+    choices=['Santos','Vasco','Flamengo','Corinthians','Palmeiras']
+)if campeao_br == 'Palmeiras':
+    print('Você acertou, o Palmeiras é o maior campeão brasileiro com 13 títulos!')
+else: print('Você errou, tente novamente')'''
